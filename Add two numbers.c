12f61100af56c767807e89a6c642ff2dee084174
NaveenAgrawal44 <96571133+NaveenAgrawal44@users.noreply.github.com>
2021-12-23 19:31:02 +0530

    Add files via upload

---
+++ Add two numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int x,y,sum;
+ printf("enter your first value: ");
+ scanf("%d",&x);
+ printf("Enter your second value: ");
+ scanf("%d",&y);
+ sum=x+y;
+ printf("Sum = %d",sum);
+    return 0 ;
+}
