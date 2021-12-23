fa41e564e83b3dcc8b35bfed239b4a5d4b7e0419
NaveenAgrawal44 <96571133+NaveenAgrawal44@users.noreply.github.com>
2021-12-23 19:07:03 +0530

    Add files via upload

---
+++ Print an integer.c
@@ -0,0 +1,15 @@
+// c program to print an integer
+
+#include <stdio.h>
+int main()
+ {   
+    int number;
+   
+    printf("Enter an integer: ");  
+   
+    scanf("%d", &number);
+    
+    printf("Your enter integer is %d", number);
+    
+    return 0;
+}
